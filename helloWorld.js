6ce0d76d34017760fcfb35e590118a79a830777d
Adi Asaf <adiasaf181@gmail.com>
2023-01-10 15:38:47 +0200

    first commit

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+function buttonFunction(){
+    
+    let MyDiv= document.getElementById("MyIdDiv");
+
+    MyDiv.innerHTML += 'Hello World!';
+    MyDiv.style.fontFamily="'Dosis', sans-serif"
+    MyDiv.style.fontSize="40px";
+    MyDiv.style.textAlign="center";
+}
